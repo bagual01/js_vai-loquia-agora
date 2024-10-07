3695a84abfe1d37fe416e9d88e13306457a2d909
bagual01 <joao.lehrbaum@escola.pr.gov.br>
2024-10-07 13:35:25 -0300

    adicionando arquvos js e css

---
+++ sketch.js
@@ -0,0 +1,18 @@
+
+function setup() {
+  createCanvas(1000, 1000);
+  background("black");
+}
+
+function draw() {
+
+
+  stroke("blue");
+  fill("red");
+
+  // console.log(mouseIsPressed)
+
+  if (mouseIsPressed) {
+    rect(mouseX, mouseY, 25, 35);
+  }
+}
